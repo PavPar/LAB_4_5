4a7f7c928dd9cdf2b5b67d6a59e8a56481047eea
PavPar <ParamonPav@yandex.ru>
2020-04-23 22:10:35 +0300

    feat:Added basic structure

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
